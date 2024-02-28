64ee9ff93d3c859783a7d1da9d3d2f5a69fc59fa
Facundo Procelli <facundoprocelli@gmail.com>
2024-02-28 12:40:08 -0300

    agrege las libreris de listas y la funcion sumar

---
+++ curso-git-repo-local/listas.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int sumar(int a, int b)
+{
+    int c = a + b;
+    return c;
+}
--- curso-git-repo-local/main.c
+++ curso-git-repo-local/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "listas.h"
 
 int main()
 {
     int a = 1;
     int b = 2;
 
-    int c = a + b;
+    int c = suma(a, b);
+    
     printf("%i", c);
     printf("Hello world!\n");
     return 0;
